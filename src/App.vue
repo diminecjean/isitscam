8c4ec3911b33e544cc73e3d7e50bd8f224094c95
Looi Wei En <looi.weien02@student.usm.my>
2024-03-24 21:15:27 +0800

    Updates

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex align-middle justify-center mt-10">
-    <router-view></router-view>
-  </div>
-</template>
-
-<script></script>
-
-<style></style>
--- src/constants/color.js
+++
@@ -1,7 +0,0 @@
-export default {
-  primary: '#102E4A', // Dark blue
-  secondary: '#15A1CE', // weird shade of blue
-  background: '#EBF9FE', // Light Blue
-  red: '#9A0000',
-}
-
